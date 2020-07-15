e5cd70335d9043a41c4c9d2ec55dea59d3450214
Ierofantis <evil69669@gmail.com>
2020-07-15 16:30:57 +0300

    View movies by user when you are logged in!

--- routes/findMoviesByUser.js
+++ routes/findMoviesByUser.js
@@ -7,9 +7,12 @@ var moment = require('moment');
 /* GET user's page. */
 router.get('/:userId', async (req, res) => {
     let UserId = req.params.userId
-    let movies = await service.findMoviesByUser(UserId);
-
-    res.render('findMoviesByUser', { user: req.user, session: req.session, movies: movies, moment: moment });
+    if (req.user) {
+        let movies = await service.findMoviesByUser(UserId);
+        res.render('findMoviesByUser', { user: req.user, session: req.session, movies: movies, moment: moment });
+    } else {
+        res.send("You have to be logged in! ")
+    }
 });
 
 module.exports = router;
