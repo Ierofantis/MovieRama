a6c8cbe75fe25bf4d42b5beb2390a33bedd88228
Ierofantis <evil69669@gmail.com>
2020-07-18 14:07:01 +0300

    Cleaner Code

--- services/conditionsForLikesAndHates.js
+++ services/conditionsForLikesAndHates.js
@@ -1,14 +1,35 @@
-const genericService = require("../services/genericUpdater");
+const service = require("../services/movieService");
 
 exports.conditionsForLikesAndHates = (reqMovie, check, checkFromDb, likeCounter, hateCounter) => {
 
-    if (typeof check !== "undefined") {
-        if (checkFromDb[Object.keys(checkFromDb)[0]]) {
-            genericService.genericHateTrueUpdater(reqMovie, check.likeBool, checkFromDb[Object.keys(checkFromDb)[0]], "like", likeCounter, hateCounter)
-        } else if (checkFromDb[Object.keys(checkFromDb)[1]]) {
-            genericService.genericHateTrueUpdater(reqMovie, check.hateBool, checkFromDb[Object.keys(checkFromDb)[1]], "hate", likeCounter, hateCounter)
+    try {
+        if (typeof check !== "undefined") {
+            if (checkFromDb[Object.keys(checkFromDb)[0]] === true & check.likeBool === false) {
+                let subtractLike = likeCounter - 1;
+                service.updateLikesAndHatesFromDb(reqMovie, subtractLike, hateCounter);
+            }
+            else if (checkFromDb[Object.keys(checkFromDb)[0]] === false & check.likeBool === true || checkFromDb[Object.keys(checkFromDb)[0]] === false & check.likeBool === false) {
+                let sumLike = likeCounter + 1;
+                service.updateLikesAndHatesFromDb(reqMovie, sumLike, hateCounter);
+            }
+
+            if (checkFromDb[Object.keys(checkFromDb)[1]] === true & check.hateBool === false) {
+                let subtractHate = hateCounter - 1;
+                service.updateLikesAndHatesFromDb(reqMovie, likeCounter, subtractHate);
+            }
+            else if (checkFromDb[Object.keys(checkFromDb)[1]] === false & check.hateBool === true || checkFromDb[Object.keys(checkFromDb)[1]] === false & check.hateBool === false) {
+                let sumHate = hateCounter + 1;
+                service.updateLikesAndHatesFromDb(reqMovie, likeCounter, sumHate);
+            }
+            else {
+                console.log(">> Error on conditionsForLikesAndHates");
+            }
+        } else {
+            console.log('>> conditionsForLikesAndHates undefined')
         }
-    } else {
-        console.log(">> Error on conditionsForLikesAndHates");
+    }
+    catch (error) {
+        console.log('>> conditionsForLikesAndHates error', error)
+        throw error
     }
 };
